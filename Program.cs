9a593b5bac8e32b450a2a9d17c92dabe89aecfe7
maxitect <maxime.downe@gmail.com>
2024-11-05 21:19:24 +0000

    feat: module 3 challenge complete

--- Program.cs
+++ Program.cs
@@ -1,40 +1,24 @@
-﻿Random dice = new Random();
+﻿Random random = new Random();
+int daysUntilExpiration = 4;
+int discountPercentage = 0;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if (daysUntilExpiration == 0)
 {
-    if ((roll1 == roll2) && (roll2 == roll3))
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6;
-    }
-    else
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+    Console.WriteLine("Your subscription has expired.");
 }
-
-if (total >= 16)
+else if (daysUntilExpiration == 1)
 {
-    Console.WriteLine("You win a new thingymajib!");
+    discountPercentage = 20;
+    Console.WriteLine($@"Your subscription expires within a day!
+Renew now and save {discountPercentage}%!");
 }
-else if (total >= 10)
+else if (daysUntilExpiration <= 5)
 {
-    Console.WriteLine("You win a new majibmacthingy!");
+    discountPercentage = 10;
+    Console.WriteLine($@"Your subscription expires in {daysUntilExpiration} days.
+Renew now and save {discountPercentage}%!");
 }
-else if (total == 7)
+else if (daysUntilExpiration <= 10)
 {
-    Console.WriteLine("You win a trip to Blackpool tower!");
-}
-else
-{
-    Console.WriteLine("You get nothing!");
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
 }
