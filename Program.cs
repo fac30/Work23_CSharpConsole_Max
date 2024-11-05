d2c3ff4fefc2abf0f6e25cd69e0f36b7c669604d
maxitect <maxime.downe@gmail.com>
2024-11-05 22:04:29 +0000

    feat: module 5 challenge on making code more readable

--- Program.cs
+++ Program.cs
@@ -1,22 +1,21 @@
-﻿Random dice = new Random();
+﻿string str = "The quick brown fox jumps over the lazy dog.";
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
 
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+// count the o's
+foreach (char i in charMessage)
 {
-    if ((roll1 == roll2) && (roll2 == roll3)) 
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6; 
-    } 
-    else 
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
+    if (i == 'o') { x++; }
 }
+
+// convert it back to a string
+string new_message = new(charMessage);
+
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
